ddb20911f8f316923f375acc8173c60fe2a0b85a
HarvieHeavenz <heavenzharvey288@gmail.com>
2021-04-12 18:52:52 +0200

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 account_sid = 'AC1aca673df2901e1bf97f6f46a5523878'
 auth_token = '4707a2e804cd23c0f0788a78479d0977'
-my_cell = "+260979181334"
+my_cell = "Your_Number_Goes_Here"
 my_twilio =  "+12014313960"
